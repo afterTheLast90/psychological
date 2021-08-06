f194b5c67c55cc8629894a069e6b5d3d1d7c2733
DELL <wxlikerock@163.com>
2021-08-07 00:11:14 +0800

    初始化仓库

---
+++ src/main/java/team/cats/psychological/PsychologicalApplication.java
@@ -0,0 +1,13 @@
+package team.cats.psychological;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/cats/psychological/PsychologicalApplicationTests.java
@@ -0,0 +1,13 @@
+package team.cats.psychological;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
