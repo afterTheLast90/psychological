8019b25d660338a3831b219d7b4409b1e4ac6bcb
DELL <wxlikerock@163.com>
2021-10-29 17:43:09 +0800

    超管查地区bug修复

--- src/main/java/team/cats/psychological/service/AreaService.java
+++ src/main/java/team/cats/psychological/service/AreaService.java
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Service;
 import team.cats.psychological.base.BaseException;
 import team.cats.psychological.base.BasePageParam;
 import team.cats.psychological.base.PageResult;
-import team.cats.psychological.base.R;
-import team.cats.psychological.entity.*;
+import team.cats.psychological.entity.Area;
+import team.cats.psychological.entity.School;
+import team.cats.psychological.entity.Users;
 import team.cats.psychological.mapper.AreaMapper;
 import team.cats.psychological.mapper.SchoolMapper;
 import team.cats.psychological.mapper.UsersMapper;
@@ -16,7 +17,6 @@ import team.cats.psychological.param.AreaParams;
 import team.cats.psychological.vo.AreaAndUser;
 import team.cats.psychological.vo.City;
 import team.cats.psychological.vo.Province;
-import team.cats.psychological.vo.StudentView;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -34,11 +34,14 @@ public class AreaService {
     @Resource
     private SchoolMapper schoolMapper;
 
-    public void insertArea(AreaParams areaParams){
-        Area area=new Area();
+    public void insertArea(AreaParams areaParams) {
+        Area area = new Area();
         Area area1 = areaMapper.selectById(areaParams.getAreaId());
-        if (area1!=null){
-            throw new BaseException(400,"该地区已存在");
+        if (area1 != null) {
+            throw new BaseException(400, "该地区已存在");
+        }
+        if (areaParams.getPrincipal().equals(100)){
+            areaParams.setPrincipal(StpUtil.getLoginIdAsLong());
         }
         area.setAreaId(areaParams.getAreaId());
         area.setAreaName(areaParams.getAreaName());
@@ -48,31 +51,31 @@ public class AreaService {
         area.setProvinceName(areaParams.getProvinceName());
         area.setAreaPrincipal(areaParams.getPrincipal());
         int insert = areaMapper.insert(area);
-        if (insert==0){
-            throw new BaseException(400,"插入失败");
+        if (insert == 0) {
+            throw new BaseException(400, "插入失败");
         }
     }
 
     public void deleteArea(Long areaId) {
-        Area area =getAreaNotNull(areaId);
+        Area area = getAreaNotNull(areaId);
         QueryWrapper<School> schoolQueryWrapper = new QueryWrapper<>();
-        schoolQueryWrapper.eq("area_id",area.getAreaId());
+        schoolQueryWrapper.eq("area_id", area.getAreaId());
         List<School> schoolList = schoolMapper.selectList(schoolQueryWrapper);
-        if (schoolList.size()>0){
-            throw new BaseException(400,"地区中还有学校，不能删除");
+        if (schoolList.size() > 0) {
+            throw new BaseException(400, "地区中还有学校，不能删除");
         }
         areaMapper.deleteById(areaId);
     }
 
-    public Area getAreaNotNull(Long areaId){
+    public Area getAreaNotNull(Long areaId) {
         Area area = areaMapper.selectById(areaId);
-        if(area==null){
+        if (area == null) {
             throw new BaseException(400, "地区不存在");
         }
         return area;
     }
 
-    public List<Area> getArea(){
+    public List<Area> getArea() {
         List<Area> areaList = new ArrayList<>();
 
         long userId = StpUtil.getLoginIdAsLong();
@@ -80,47 +83,58 @@ public class AreaService {
         Long userRole = users.getUserRole();
         if (userRole == 0) {
             QueryWrapper<Area> queryWrapper = new QueryWrapper<>();
-            queryWrapper.eq("delete_flag",0);
+            queryWrapper.eq("delete_flag", 0);
             List<Area> areas = areaMapper.selectList(queryWrapper);
-            areaList=areas;
+            areaList = areas;
         } else if (userRole == 1) {
             QueryWrapper<Area> areaQueryWrapper = new QueryWrapper<>();
             areaQueryWrapper.eq("area_principal", userId);
             List<Area> areas = areaMapper.selectList(areaQueryWrapper);
-            areaList=areas;
+            areaList = areas;
         }
         return areaList;
     }
 
-    public List<City> getCity(){
+    public List<City> getCity() {
         List<City> cities = areaMapper.selectCity();
         return cities;
     }
 
-    public List<Province> getProvince(){
+    public List<Province> getProvince() {
         List<Province> provinces = areaMapper.selectProvince();
         return provinces;
     }
 
-    public PageResult<AreaAndUser> getAreaUser(BasePageParam basePageParam, String value, Long cityId, Long provinceId){
+    public PageResult<AreaAndUser> getAreaUser(BasePageParam basePageParam, String value, Long cityId, Long provinceId) {
 
 
-        PageHelper.startPage(basePageParam.getPageNum(),basePageParam.getPageSize());
+        PageHelper.startPage(basePageParam.getPageNum(), basePageParam.getPageSize());
+        List<AreaAndUser> areaAndUsers = null;
         long userId = StpUtil.getLoginIdAsLong();
-        List<AreaAndUser> areaAndUsers =areaMapper.selectAreaUsers(value,cityId,provinceId,userId);
-        for (AreaAndUser areaAndUser : areaAndUsers) {
-            areaAndUser.setSchool(schoolMapper.selectByAreaId(areaAndUser.getAreaId()));
+        Users users = usersMapper.selectById(userId);
+        Long userRole = users.getUserRole();
+        if (userRole == 0) {
+            userId = 0;
+            areaAndUsers = areaMapper.selectAreaUsers(value, cityId, provinceId, userId);
+            for (AreaAndUser areaAndUser : areaAndUsers) {
+                areaAndUser.setSchool(schoolMapper.selectByAreaId(areaAndUser.getAreaId()));
+            }
+        } else if (userRole == 1) {
+            areaAndUsers = areaMapper.selectAreaUsers(value, cityId, provinceId, userId);
+            for (AreaAndUser areaAndUser : areaAndUsers) {
+                areaAndUser.setSchool(schoolMapper.selectByAreaId(areaAndUser.getAreaId()));
+            }
         }
         return new PageResult<AreaAndUser>(areaAndUsers);
     }
 
-    public void editPrincipal(Long areaId,Long areaPrincipal){
+    public void editPrincipal(Long areaId, Long areaPrincipal) {
         Area area = getAreaNotNull(areaId);
         System.out.println(area);
         area.setAreaPrincipal(areaPrincipal);
         int i = areaMapper.updateById(area);
-        if(i==0){
-            throw new BaseException(400,"更新失败");
+        if (i == 0) {
+            throw new BaseException(400, "更新失败");
         }
     }
 }
